9b9ce271b193eabb18390a7ffc9e999defd06443
Alan Foster <alan@alanfoster.me>
2015-01-24 03:05:46 +0000

    Trigger alert on browserify errors. There may be a cleaner way to do this

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var replace = require('gulp-replace');
+var rename = require('gulp-rename');
 var haml = require('gulp-haml');
 var karma = require('karma').server;
+
+var handleError = function(error) {
+    var errorContent = escape(error.message.replace(/'|"/g, "\'"))
+
+
+    gulp.src('app/assets/js/error.js')
+        .pipe(replace("#{ERROR}", errorContent))
+        .pipe(rename('main.js'))
+        .pipe(gulp.dest('dist/js'));
+};
 
 gulp.task('browserify', function () {
     gulp.src('app/assets/js/main.js')
         .pipe(browserify())
+        .on('error', handleError)
         .pipe(gulp.dest('dist/js'));
 });
 
