67bc01d0f5e4c7faedfc052fc3c8d17dddcb3392
Alan Foster <alan@alanfoster.me>
2015-01-25 14:17:30 +0000

    Add initial render system

---
+++ app/assets/js/systems/render.js
@@ -0,0 +1,25 @@
+var Render = (function() {
+    var System = function() {
+
+    };
+
+    System.prototype = {
+        update: function(entities) {
+            var process = this.process;
+            entities.forEach(function(entity) {
+                var rendered = entity.getComponent('rendered');
+                if (!rendered) return;
+
+                process(entity, { rendered: rendered })
+            })
+        },
+        process: function(entity, components) {
+            // TODO
+            window.document.body.innerHTML = entity.toString();
+        }
+    };
+
+    return System;
+})();
+
+exports.Render = Render;
---
+++ spec/assets/js/systems/render_spec.js
@@ -0,0 +1,57 @@
+var subject = require('./../../../../app/assets/js/systems/render').Render;
+var Entity = require('./../../../../app/assets/js/entities/entity').Entity;
+var Location = require('./../../../../app/assets/js/components/location').Location;
+var Rendered = require('./../../../../app/assets/js/components/rendered').Rendered;
+
+describe('Movement System', function() {
+    it('exists', function() {
+        expect(subject).toBeTruthy();
+    });
+
+    describe('#update', function(){
+        var instance = new subject();
+
+        it('exists', function() {
+            expect(instance.update).toBeDefined();
+        });
+
+        context('when no entities are supplied', function(){
+            it('does not cause an error', function() {
+                expect(function() { instance.update([]) } ).not.toThrow();
+            })
+        });
+
+        context('entities with components supplied', function(){
+            var rendered = new Rendered();
+            var validEntity = new Entity('valid').addComponent(rendered)
+                                                 .addComponent(new Location());
+
+            var invalidEntity1 = new Entity('invalid1');
+
+            beforeEach(function() {
+                spyOn(instance, 'process');
+                var entities = [
+                    invalidEntity1,
+                    validEntity
+                ];
+                instance.update(entities);
+            });
+
+            it('called processed the required entities', function(){
+                var expectedComponents = {
+                    rendered: rendered
+                };
+
+                expect(instance.process).toHaveBeenCalledWith(validEntity, expectedComponents);
+            });
+
+            it('did not process invalid entity 1', function() {
+                expect(instance.process).not.toHaveBeenCalledWith(invalidEntity1, jasmine.any(Object));
+            });
+        })
+    });
+
+    describe('#process', function() {
+      // TODO - Decide how to best test the DOM ...
+    });
+});
